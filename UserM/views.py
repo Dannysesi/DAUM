fcaee017f7f5efe28d26eb9ec555166efcab2cb8
Dannysesi <daniel.olusesi20@gmail.com>
2024-03-25 05:58:39 +0100

    view modification

--- UserM/views.py
+++ UserM/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from .forms import *
 from .models import *
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.decorators import login_required
-# from .models import *
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.models import User, Group
 
 
 
@@ -15,13 +15,34 @@ def home(request):
 
     if request.method == 'POST':
         post_id = request.POST.get('post-id')
-        post = Post.objects.filter(id=post_id).first()
-        if post and post.author == request.user:
-            post.delete()
+        user_id = request.POST.get('user-id')
+
+        if post_id:
+            post = Post.objects.filter(id=post_id).first()
+            if post and (post.author == request.user or request.user.has_perm('UserM.delete_post')):
+                post.delete()
+        elif user_id:
+            user = User.objects.filter(id=user_id).first()
+            if user and request.user.is_staff:
+                try:
+                    group_default = Group.objects.get(name='default')
+                    group_default.user_set.remove(user)
+                except Group.DoesNotExist:
+                    pass
+
+                try:
+                    group_mod = Group.objects.get(name='mod')
+                    group_mod.user_set.remove(user)
+                except Group.DoesNotExist:
+                    pass
+
+
+        
 
     return render(request, 'UserM/home.html', {'posts': posts})
 
 @login_required(login_url='/login')
+@permission_required('UserM.add_post', login_url='/login', raise_exception=True)
 def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
